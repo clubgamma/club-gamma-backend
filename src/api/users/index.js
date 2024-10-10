a13c64a9b2221641fe9c3e1f03e330f597847e66
Nishant <mojonly813@gmail.com>
2024-10-10 14:54:29 +0530

    Added RateLimiting Api and JWTVerification to route /api/users/sync-prs

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const controller = require('./controller');
-
+const { rateLimiting } = require('../../utils/Middlewares');
 const router = Router();
 
 router.get('/stats/:githubId', controller.getUserStats);
-router.post('/sync-prs', controller.syncPullRequests);
+router.post('/sync-prs', verifyJWT , rateLimiting  ,controller.syncPullRequests);
 
 module.exports = router;
