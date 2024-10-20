e4f7e41d5880bfe9c52e2a79f74212d3a09d8683
Krishna Awasthi <awasthikrishna23052005@gmail.com>
2024-10-20 20:36:07 +0530

    feat:added the API endpoint /api/projects/contributors/:id

--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ const authRouter = require('./api/auth/index');
 const leaderboardRouter = require('./api/leaderboard/index');
 const userRouter = require('./api/users/index');
 const statsRouter = require('./api/stats/index');
+const projectsRouter = require('./api/project/index');
 
 
 function routes(app) {
     app.use('/api/auth', authRouter);
     app.use('/api/leaderboard', leaderboardRouter);
     app.use('/api/users', userRouter);
-    app.use('/api/stats', statsRouter)
+    app.use('/api/stats', statsRouter);
+    app.use('/api/projects', projectsRouter);
 }
 
 module.exports = routes;
