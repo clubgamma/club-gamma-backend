daba1fa6dc6f3b8a9e16d83f18ac7f143534b053
soni-shashan <lumbhanishashan1510@gmail.com>
2024-10-26 19:07:06 +0530

    Update index.js

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,6 +4,7 @@ const { rateLimiting , verifyJWT } = require('../../utils/Middlewares');
 const router = Router();
 
 router.get('/stats/:githubId', controller.getUserStats);
+router.get('/contributors/:githubId', controller.getProjectWiseContributions);
 router.post('/sync-prs', verifyJWT , rateLimiting, controller.syncPullRequests);
 
 module.exports = router;
