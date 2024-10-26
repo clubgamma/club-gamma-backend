195b4b8aaba7b956fc9f8325758bc5bd359a51e3
soni-shashan <lumbhanishashan1510@gmail.com>
2024-10-26 19:25:20 +0530

    project wise contribution in stats

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,7 +4,6 @@ const { rateLimiting , verifyJWT } = require('../../utils/Middlewares');
 const router = Router();
 
 router.get('/stats/:githubId', controller.getUserStats);
-router.get('/contributors/:githubId', controller.getProjectWiseContributions);
 router.post('/sync-prs', verifyJWT , rateLimiting, controller.syncPullRequests);
 
 module.exports = router;
